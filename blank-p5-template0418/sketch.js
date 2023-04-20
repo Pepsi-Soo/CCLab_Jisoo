34188ebb14112bbbdf7335fefa9bb2eb35e5dc26
Pepsi-Soo <sj3950@nyu.edu>
2023-04-20 14:02:08 +0800

    Initial commit

---
+++ blank-p5-template0418/sketch.js
@@ -0,0 +1,7 @@
+function setup(){
+  let cnv = createCanvas(400,400);
+  cnv.parent("canvasContainer");
+}
+function draw(){
+    background(120,40,240);
+}
---
+++ bring-js-back0418-1/jisoo.js
@@ -0,0 +1,4 @@
+// let headline =document.getElementById("myHeadline");
+// let theButton = document.getElementById("mybotton");
+// // 写不完回去看笔记
+
